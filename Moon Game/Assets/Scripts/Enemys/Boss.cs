9bcca4d15ade4571f0be74442152c50bd6a0ccbe
gmeyerp <gmeyerp@gmail.com>
2024-04-20 19:16:19 -0300

    Finalizacao de script e vfx do boss
    Para implementação final ainda desenvolver algumas variações no prefab das armadilhas

--- Moon Game/Assets/Scripts/Enemys/Boss.cs
+++ Moon Game/Assets/Scripts/Enemys/Boss.cs
@@ -14,6 +14,11 @@ public class Boss : MonoBehaviour
     [SerializeField] float playerDelta = 15f;
     [SerializeField] float floatSpeed = 0.5f;
 
+    [Header("Damaged")]
+    [SerializeField] float fallSpeed = 0.15f;
+    [SerializeField] int health = 3;
+    [SerializeField] float defeatDelay = 2f;
+
     [Header("Attack")]
     [SerializeField] float drainEffectTimer = 1.5f;
     [SerializeField] float drainFinishTimer = 1.5f;
@@ -24,6 +29,9 @@ public class Boss : MonoBehaviour
     [SerializeField] ParticleSystem hitVFX;
     [SerializeField] ParticleSystem drainVFX;
     [SerializeField] ParticleSystem trapVFX;
+    [SerializeField] AudioClip damagedClip;
+    [SerializeField] AudioClip drainSFX;
+    [SerializeField] AudioClip trapSFX;
 
     void Start()
     {
@@ -36,14 +44,25 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isDamaged)
+        if (isDamaged)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, 0, 0);
+            transform.position = Vector3.Lerp(transform.position, newPos, fallSpeed);
+        }
+        else
         {
             Vector3 newPos = new Vector3(player.transform.position.x + playerDelta, heightPosition + Mathf.Sin(Time.time * 3.14f), transform.position.z);
             transform.position = Vector3.Lerp(transform.position, newPos, floatSpeed);
         }
+
+        if (player.transform.position.x - transform.position.x > 5f)
+        {
+            Recover();
+        }
     }
-    void DrainLight()
+    public void DrainLight()
     {
+        SoundManager.instance.PlaySFX(drainSFX);
         drainVFX.Play();
         StartCoroutine(CDrainTimer(drainEffectTimer, drainFinishTimer));
     }
@@ -71,11 +90,48 @@ public class Boss : MonoBehaviour
 
     void ActivateTraps()
     {
+        SoundManager.instance.PlaySFX(trapSFX);
         trapVFX.Play();
         StartCoroutine(CTrapsTimer(trapsEffectTimer));
+    }    
+
+    public void Fall()
+    {
+        isDamaged = true;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isDamaged && (player.isDashingH || player.isDashingV))
+        {
+            TakeDamage();
+        }
+    }
 
+    void TakeDamage()
+    {
+        hitVFX.Play();
+        health --;
+        SoundManager.instance.PlaySFX(damagedClip);
+        if (health <= 0)
+        {
+            StartCoroutine(CBossDefeated());
+        }
+        else
+        {
+            Recover();
+        }
+    }
 
-    
+    void Recover()
+    {
+        isDamaged = false;
+        ActivateTraps();
+    }
+
+    IEnumerator CBossDefeated()
+    {
+        yield return new WaitForSeconds(defeatDelay);
+        GameManager.instance.PlayerWin();
+    }
 }
